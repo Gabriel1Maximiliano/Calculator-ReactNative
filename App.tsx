3187e988766d27735ea5ba459b923c0d77c41fac
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-04-28 20:15:59 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+export const App = () => {
+  return (
+    <View>
+      <Text style={ {fontSize:45} } >ettxtt</Text>
+
+    </View>
+  )
+}
